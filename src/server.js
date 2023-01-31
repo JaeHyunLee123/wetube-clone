06fbfcd97d110e3a10d32f2aec7942777fd2d118
JaehyunLee123 <jhyon123@gmail.com>
2023-01-31 20:40:48 +0900

    first mongoose model

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+import "./db.js";
+import Video from "./models/Video";
+
 import express from "express";
 import morgan from "morgan";
 
@@ -19,7 +22,7 @@ app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 const handleListening = () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listening on port http://localhost:${PORT}`);
 };
 
 app.listen(PORT, handleListening);
