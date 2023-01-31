c8efc211fa675e13d7c8b26ea179722d6cecf287
JaehyunLee123 <jhyon123@gmail.com>
2023-01-31 21:53:38 +0900

    CRUD - how to read

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,50 +1,27 @@
-let videos = [
-  {
-    title: "First",
-    raiting: 5,
-    comments: 2,
-    createdAt: "1 hours ago",
-    views: 1110,
-    id: 0,
-  },
-  {
-    title: "Second",
-    raiting: 5,
-    comments: 2,
-    createdAt: "1 hours ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Third",
-    raiting: 5,
-    comments: 2,
-    createdAt: "1 hours ago",
-    views: 59,
-    id: 2,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch {
+    return res.send("server-error");
+  }
 };
 
 export const search = (req, res) => res.send("search");
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id];
-  return res.render("watch", {
-    pageTitle: `Watching: ${video.title}`,
-    video,
-  });
+
+  return res.render("watch");
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id];
   return res.render("edit", {
-    pageTitle: `Editing: ${video.title} video`,
+    pageTitle: `Editingvideo`,
     video,
   });
 };
@@ -52,7 +29,6 @@ export const getEdit = (req, res) => {
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -64,16 +40,6 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   // her we will add a video a video to the videos array
-  const newVideo = {
-    title: req.body.title,
-    raiting: 0,
-    comments: 0,
-    createdAt: "1 minute ago",
-    views: 0,
-    id: videos.length,
-  };
-
-  videos.push(newVideo);
 
   return res.redirect("/");
 };
