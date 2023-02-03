b87d4a9e5c86b89c0775224ba310f9dc9e5eb8dc
JaehyunLee123 <jhyon123@gmail.com>
2023-02-03 22:16:14 +0900

    #6.19 video detail

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,10 +11,10 @@ export const home = async (req, res) => {
 
 export const search = (req, res) => res.send("search");
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("watch");
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const getEdit = (req, res) => {
