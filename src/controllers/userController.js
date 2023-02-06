94fac500377dc91713d4b14ae1db4c7be898adfc
JaehyunLee123 <jhyon123@gmail.com>
2023-02-06 09:46:33 +0900

    #7.0~7.3 Create account

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,20 @@
+import User from "../models/User";
 
-
-export const join = (req, res) => res.send("Join");
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = async (req, res) => {
+  console.log(req.body);
+  const { name, email, username, password, location } = req.body;
+  await User.create({
+    name,
+    email,
+    username,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.send("Edit user");
 export const deleteAccount = (req, res) => res.send("delete user");
