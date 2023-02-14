0ab891a10e68ab1aea7f05933e499a58cfebcd23
JaehyunLee123 <jhyon123@gmail.com>
2023-02-14 17:33:17 +0900

    11.1 play/pause button

--- src/frontend/js/videoPlayer.js
+++ src/frontend/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("Video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (event) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+
+const handleMute = (event) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
