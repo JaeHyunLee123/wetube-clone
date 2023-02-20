dcdd3c586f67e675c19b47b7527cb1a1094c88a8
JaehyunLee123 <jhyon123@gmail.com>
2023-02-20 21:24:33 +0900

    16.7 realtime ids

--- src/frontend/js/commentSection.js
+++ src/frontend/js/commentSection.js
@@ -1,6 +1,23 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoCommnents = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+
+  const icon = document.createElement("i");
+  icon.className = "fa-regular fa-comment";
+
+  const span = document.createElement("span");
+  span.innerText = text;
+
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.className = "video__comment";
+
+  videoCommnents.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
 
@@ -13,15 +30,19 @@ const handleSubmit = async (event) => {
     return;
   }
 
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
+
+  if (status === 201) {
+    addComment(text);
+  }
+
   textarea.value = "";
-  window.location.reload();
 };
 
 if (form) {
