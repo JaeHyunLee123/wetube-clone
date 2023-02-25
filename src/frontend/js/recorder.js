da6bcfb5cc47c7cf5dbd61c1c295bd5a1d5706a6
JaeHyunLee123 <jhyon123@gmail.com>
2023-02-25 17:00:57 +0900

    13.3 recording video part two

--- src/frontend/js/recorder.js
+++ src/frontend/js/recorder.js
@@ -2,26 +2,33 @@ const recordBtn = document.getElementById("recordBtn");
 const preview = document.getElementById("preview");
 
 let stream = null;
+let recorder = null;
 
 const handleStart = () => {
   recordBtn.innerText = "Stop recording";
   recordBtn.removeEventListener("click", handleStart);
   recordBtn.addEventListener("click", handleStop);
 
-  const recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     //Can get video from event.data
+    const videoFile = URL.createObjectURL(event.data);
+    //Set preview's source to recorded video
+    preview.srcObject = null;
+    preview.src = videoFile;
+    //Show recorded video
+    preview.loop = true;
+    preview.play();
   };
   recorder.start();
-  setTimeout(() => {
-    recorder.stop();
-  }, 5000);
 };
 
 const handleStop = () => {
   recordBtn.innerText = "Start recoring";
   recordBtn.removeEventListener("click", handleStop);
   recordBtn.addEventListener("click", handleStart);
+
+  recorder.stop();
 };
 
 const init = async () => {
