e982baa0cdd7b508785018a62fdf329892c6a7c8
JaeHyunLee123 <jhyon123@gmail.com>
2023-02-25 16:38:41 +0900

    13.2 recording video

--- src/frontend/js/recorder.js
+++ src/frontend/js/recorder.js
@@ -1,13 +1,38 @@
 const recordBtn = document.getElementById("recordBtn");
 const preview = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream = null;
+
+const handleStart = () => {
+  recordBtn.innerText = "Stop recording";
+  recordBtn.removeEventListener("click", handleStart);
+  recordBtn.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    //Can get video from event.data
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+
+const handleStop = () => {
+  recordBtn.innerText = "Start recoring";
+  recordBtn.removeEventListener("click", handleStop);
+  recordBtn.addEventListener("click", handleStart);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: { width: 600 },
   });
   preview.srcObject = stream;
   preview.play();
 };
+
+init();
 
 recordBtn.addEventListener("click", handleStart);
