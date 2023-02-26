915e42e1bbf84fcce00abc6da1de7396080cbe68
JaeHyunLee123 <jhyon123@gmail.com>
2023-02-26 21:17:59 +0900

    fix: make downloadFile function and files object

--- src/frontend/js/recorder.js
+++ src/frontend/js/recorder.js
@@ -7,29 +7,43 @@ let stream = null;
 let recorder = null;
 let videoFile = null;
 
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, filename) => {
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = filename; //filename.fileformat
+  document.body.appendChild(a);
+  a.click();
+};
+
 const handleDownload = async () => {
   //create a ffmpeg object and load
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
   //make a file to convert
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
   //this function run ffmpeg code that use in terminal
   //this fucntion convert recording.wemb to output.mp4 and encode 60 frames for a sec
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
   //this function capture the video at the moment of "00:00:01"
   await ffmpeg.run(
     "-i",
-    "recording.webm",
+    files.input,
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    "thumbnail.jpg"
+    files.thumb
   );
 
   //this is raw data consists of unsigned integer array
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
   //this is real video and jpg
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
   const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
@@ -37,21 +51,12 @@ const handleDownload = async () => {
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement("a");
-  a.href = mp4Url;
-  a.download = "MyRecord.mp4"; //filename.fileformat
-  document.body.appendChild(a);
-  a.click();
+  downloadFile(mp4Url, "MyRecord.mp4");
+  downloadFile(thumbUrl, "MyThumbnail.jpg");
 
-  const thumbA = document.createElement("a");
-  thumbA.href = thumbUrl;
-  thumbA.download = "MyThumbnail.jpg"; //filename.fileformat
-  document.body.appendChild(thumbA);
-  thumbA.click();
-
-  ffmpeg.FS("unlink", "recording.webm");
-  ffmpeg.FS("unlink", "output.mp4");
-  ffmpeg.FS("unlink", "thumbnail.jpg");
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumb);
 
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
